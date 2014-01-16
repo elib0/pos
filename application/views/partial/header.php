699d8ed09987f267a387452324ad71b2c24f259f
elibo <elijose.c@gmail.com>
2014-01-16 16:59:08 -0430

    Jquery PlugIn For Alert Message

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -8,8 +8,10 @@
 	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/ospos.css" />
 	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/ospos_print.css"  media="print"/>
 	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/jquery-ui.css" />
+	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/notifIt.css" />
 	<script>BASE_URL = '<?php echo site_url(); ?>';</script>
 	<script src="<?php echo base_url();?>js/jquery-1.10.2.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+	<script src="<?php echo base_url();?>js/notifIt.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
 	<script src="<?php echo base_url();?>js/jquery-1.2.6.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
 	<script src="<?php echo base_url();?>js/jquery-ui.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
 	<script src="<?php echo base_url();?>js/jquery.color.js" type="text/javascript" language="javascript" charset="UTF-8"></script>

99638fcd9af017510cfa0ad9c0f79dda63988ccf
Gustavo Ocanto <gustavoocanto@gmail.com>
2014-03-26 14:09:40 -0430

    update login

--- application/views/login.php
+++ application/views/login.php
@@ -15,7 +15,17 @@ $(document).ready(function()
 </script>
 </head>
 <body>
-<h1>Fast i Repair Point Of Sale <?php echo $this->config->item('application_version'); ?></h1>
+
+<?php
+include('application/config/database.php'); //Incluyo donde estaran todas las config de las databses
+$dbs = array();
+foreach ($db as $key => $value) $dbs[$key] = ucwords($key); //Creo arreglo para mis <option>
+?>
+
+<div>
+	<img src="<?=base_url()?>images/logo.png" alt="" />
+</div>
+
 <?php echo form_open('login') ?>
 <div id="container">
 <?php echo validation_errors(); ?>
@@ -23,38 +33,57 @@ $(document).ready(function()
 	<?php echo $this->lang->line('login_login'); ?>
 	</div>
 	<div id="login_form">
+
 		<div id="welcome_message">
-		<?php //echo $this->lang->line('login_welcome_message'); ?>
+			<?php //echo $this->lang->line('login_welcome_message'); ?>
 		</div>
 
-		<div class="form_field_label"><?php echo $this->lang->line('login_username'); ?>: </div>
-		<div class="form_field">
-		<?php
-		echo form_input( array(
-		'name'=>'username',
-		'size'=>'20', 'value'=>$fastUser) );
-		?>
+		<div class="form-row">
+			<div class="form_field_label"><?php echo form_label('Location:', 'locationbd'); ?></div>
+			<div class="form_field"><?php echo form_dropdown('locationbd', $dbs, $_SESSION['dblocation']); ?></div>
 		</div>
 
-		<div class="form_field_label"><?php echo $this->lang->line('login_password'); ?>: </div>
-		<div class="form_field">
-		<?php echo form_password(array(
-		'name'=>'password',
-		'size'=>'20')); ?>
+		<div class="form-row">
+			<div class="form_field_label"><?php echo $this->lang->line('login_username'); ?>:</div>
+			<div class="form_field">
+				<?php
+					echo form_input(array(
+						'name'=>'username',
+						'size'=>'20', 'value'=>$fastUser
+					));
+				?>
+			</div>
 		</div>
 
-		<div id="submit_button">
-		<?php echo form_submit('loginButton','Go'); ?>
+		<div class="form-row">
+			<div class="form_field_label"><?php echo $this->lang->line('login_password'); ?>:</div>
+			<div class="form_field">
+				<?php 
+					echo form_password(array(
+						'name'=>'password',
+						'size'=>'20')
+					); 
+				?>
+			</div>
 		</div>
+		
+		<div class="form-row">
+			<?php echo form_submit('loginButton','Login', 'class = "form-button"'); ?>
+		</div>
+
+		<div class="form-row">
+			&nbsp;
+		</div>
+
 	</div>
 </div>
 <?php
-include('application/config/database.php'); //Incluyo donde estaran todas las config de las databses
-$dbs = array();
-foreach ($db as $key => $value) $dbs[$key] = ucwords($key); //Creo arreglo para mis <option>
+// include('application/config/database.php'); //Incluyo donde estaran todas las config de las databses
+// $dbs = array();
+// foreach ($db as $key => $value) $dbs[$key] = ucwords($key); //Creo arreglo para mis <option>
 
-echo form_label('Location:', 'locationbd');		//Etiqueta del helper form
-echo form_dropdown('locationbd', $dbs, $_SESSION['dblocation']);		//<select> generado con el helper
+// echo form_label('Location:', 'locationbd');		//Etiqueta del helper form
+// echo form_dropdown('locationbd', $dbs, $_SESSION['dblocation']);		//<select> generado con el helper
 echo form_close();
 ?>
 </body>

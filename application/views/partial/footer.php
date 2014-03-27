09c291326725933bb5036bf7f25545a89fbe8ce5
Miharbi Hernandez <miharbihernandez@gmail.com>
2014-03-27 15:11:37 -0430

    nada
    nada

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -5,7 +5,7 @@ $_SESSION['person_id'] = $this->session->userdata('person_id');
 ?>
 </div>
 </div>
-<div id="footer">
+<div id="footer"> 
 	Copyright &copy; Maog Host 2004 - <?=date('Y')?>. All Rights Reserved!&nbsp; 
 	<a href="http://www.maoghost.com/" target="_blank">www.maoghost.com</a>
 </div>

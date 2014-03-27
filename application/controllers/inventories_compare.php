08886f0bda5a9045a954de39d0907fdec6f1325a
elibo <elijose.c@gmail.com>
2014-03-27 16:10:33 -0430

    Ajuets correo para pruebas

--- application/controllers/inventories_compare.php
+++ application/controllers/inventories_compare.php
@@ -53,7 +53,7 @@ class Inventories_compare extends Secure_area
 
 
         $head = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0"><center>';
-        $body = $this->input->post('report');
+        $body = '<p>Hola que hace</p>';
         $footer = '</center></body></html>';
 
 

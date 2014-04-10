68a9c80f9c0a10a09f8f2d7aafdf2357f2f1d00e
ramonJR <maxtri@hotmail.com>
2014-04-10 14:07:23 -0430

    no profile customer

--- application/views/people/manage.php
+++ application/views/people/manage.php
@@ -62,13 +62,15 @@ function post_person_form_submit(response)
 <div id="title_bar">
 	<div id="title" class="float_left"><?php echo $this->lang->line('common_list_of').' '.$this->lang->line('module_'.$controller_name); ?></div>
 	<div id="new_button">
-		<?php if ($this->Employee->isAdmin()){ ?>
+		<?php 
+		if ($controller_name =='employees') {
+				if ($this->Employee->isAdmin()){ ?>
 			 		<div class='big_button' style='margin: 0px 5px;float:left;'>
 			 			<span><a href="<?php  echo site_url($controller_name."/profile_employee"); ?>" style="text-decoration: none;color: hsl(208, 45%, 41%);" >
 			 			<?php echo $this->lang->line("employees_profile_per");?></a></span>
 		 			</div>
 		<?php 	}
-		if ($controller_name =='employees') {
+		
 			if($this->Employee->has_privilege('add', $controller_name)){
 				echo anchor("$controller_name/view/-1/width:$form_width",
 				"<div class='big_button' style='float: left;'><span>".$this->lang->line($controller_name.'_new')."</span></div>",

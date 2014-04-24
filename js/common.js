8e7bdc64465307413bd5c32a269fcd032ab355de
wffranco <willemfranco@gmail.com>
2014-04-24 12:46:48 -0430

    ajuste tooltips

--- js/common.js
+++ js/common.js
@@ -1,48 +1,51 @@
-$(document).ready(function() {
-	$('.linkBack').click(function(){
-	    history.back(1);
-	    return false;
+(function($){
+	$(document).on('click','.linkBack',function(){
+		history.back(1);
+		return false;
 	});
-
-	//All Title attributes
-	$('a').hover(function(){
-            // Hover over code
-            var title = $(this).attr('title');
-            if (title) {
-            	$(this).data('tipText', title).removeAttr('title');
-	            $('<p class="tooltip"></p>')
-	            .text(title)
-	            .appendTo('body')
-	            .fadeIn('slow');
-            };
-    }, function() {
-            // Hover out code
-            $(this).attr('title', $(this).data('tipText'));
-            $('.tooltip').remove();
-    }).mousemove(function(e) {
-            var mousex = e.pageX + 20; //Get X coordinates
-            var mousey = e.pageY + 10; //Get Y coordinates
-            $('.tooltip')
-            .css({ top: mousey, left: mousex })
-    });
-});
+	//All Title attributes (tooltips)
+	$(document).on('mouseenter','[title]',function(e){
+		//mouse over (hover)
+		var title=this.title||$(this).data('title');
+		if(!title) return;
+		this.title='';
+		$(this).data('title',title);
+        var $tooltip=$('<p class="tooltip"></p>');
+        $(this).data('tooltip',$tooltip);
+        $tooltip.text(title).appendTo('body').fadeIn('slow');
+    }).on('mousemove','[title]',function(e){
+        var $tooltip=$(this).data('tooltip');
+        if(!$tooltip) return;
+        var mousex=e.pageX+20;//Get X coordinates
+        var mousey=e.pageY+10;//Get Y coordinates
+        if($tooltip.outerWidth()+mousex>=$(window).width()-5) mousex=e.pageX-10-$tooltip.outerWidth();
+        if($tooltip.outerHeight()+mousey>=$(window).height()-5) mousey=e.pageY-$tooltip.outerHeight();
+        $tooltip.css({top:mousey,left:mousex});
+    }).on('mouseleave','[title]',function(){
+		//mouse out
+        var $tooltip=$(this).data('tooltip');
+        if(!$tooltip) return;
+        $(this).data('tooltip',null);
+		$tooltip.remove();
+	});
+})(jQueryNew);
 
 function get_dimensions() 
 {
-	var dims = {width:0,height:0};
+	var dims={width:0,height:0};
 	
   if( typeof( window.innerWidth ) == 'number' ) {
-    //Non-IE
-    dims.width = window.innerWidth;
-    dims.height = window.innerHeight;
+	//Non-IE
+	dims.width=window.innerWidth;
+	dims.height=window.innerHeight;
   } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-    //IE 6+ in 'standards compliant mode'
-    dims.width = document.documentElement.clientWidth;
-    dims.height = document.documentElement.clientHeight;
+	//IE 6+ in 'standards compliant mode'
+	dims.width=document.documentElement.clientWidth;
+	dims.height=document.documentElement.clientHeight;
   } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-    //IE 4 compatible
-    dims.width = document.body.clientWidth;
-    dims.height = document.body.clientHeight;
+	//IE 4 compatible
+	dims.width=document.body.clientWidth;
+	dims.height=document.body.clientHeight;
   }
   
   return dims;
@@ -72,49 +75,49 @@ function set_feedback(text, classname, keep_displayed)
 //keylisteners
 
 $(window).jkey('f1',function(){
-window.location = BASE_URL + '/customers/index';
+window.location=BASE_URL+'/customers/index';
 });
 
 
 $(window).jkey('f2',function(){
-window.location = BASE_URL + '/items/index';
+window.location=BASE_URL+'/items/index';
 });
 
 
 $(window).jkey('f3',function(){
-window.location = BASE_URL + '/item_kits/index';
+window.location=BASE_URL+'/item_kits/index';
 });
 
 
 $(window).jkey('f4',function(){
-window.location = BASE_URL + '/suppliers/index';
+window.location=BASE_URL+'/suppliers/index';
 });
 
 $(window).jkey('f5',function(){
-window.location = BASE_URL + '/reports/index';
+window.location=BASE_URL+'/reports/index';
 });
 
 $(window).jkey('f6',function(){
-window.location = BASE_URL + '/receivings/index';
+window.location=BASE_URL+'/receivings/index';
 });
 
 
 $(window).jkey('f7',function(){
-window.location = BASE_URL + '/sales/index';
+window.location=BASE_URL+'/sales/index';
 });
 
 $(window).jkey('f8',function(){
-window.location = BASE_URL + '/employees/index';
+window.location=BASE_URL+'/employees/index';
 });
 
 $(window).jkey('f9',function(){
-window.location = BASE_URL + '/giftcards/index';
+window.location=BASE_URL+'/giftcards/index';
 });
 
 $(window).jkey('f10',function(){
-window.location = BASE_URL + '/config/index';
+window.location=BASE_URL+'/config/index';
 });
 
 $(window).jkey('f11',function(){
-window.location = BASE_URL + '/employees/assistance';
+window.location=BASE_URL+'/employees/assistance';
 });

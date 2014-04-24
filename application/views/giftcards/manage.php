a26a9d3640e10f791f000740281d9d4544074320
wffranco <willemfranco@gmail.com>
2014-04-24 13:14:53 -0430

    ajuste estilos

--- application/views/giftcards/manage.php
+++ application/views/giftcards/manage.php
@@ -8,7 +8,7 @@
 <?php echo $this->pagination->create_links();?>
 <div id="table_action_header">
 	<ul>
-		<?php if($this->Employee->has_privilege('delete', $controller_name)):  ?>
+		<?php if($this->Employee->has_privilege('delete', $controller_name)): ?>
 		<li class="float_left">
 			<?=anchor("$controller_name/delete",'<span>'.$this->lang->line("common_delete").'</span>',array('id'=>'delete'))?>
 		</li>
@@ -21,59 +21,43 @@
 		</li>
 	</ul>
 </div>
-
 <div id="table_holder">
 <?=$manage_table?>
 </div>
 <div id="feedback_bar"></div>
 <script type="text/javascript">
-$(document).ready(function()
-{
-    init_table_sorting();
-    enable_select_all();
-    enable_checkboxes();
-    enable_row_selection();
-    enable_search('<?php echo site_url("$controller_name/suggest")?>','<?php echo $this->lang->line("common_confirm_search")?>');
-    enable_delete('<?php echo $this->lang->line($controller_name."_confirm_delete")?>','<?php echo $this->lang->line($controller_name."_none_selected")?>');
-
+$(document).ready(function(){
+	init_table_sorting();
+	enable_select_all();
+	enable_checkboxes();
+	enable_row_selection();
+	enable_search('<?php echo site_url("$controller_name/suggest")?>','<?php echo $this->lang->line("common_confirm_search")?>');
+	enable_delete('<?php echo $this->lang->line($controller_name."_confirm_delete")?>','<?php echo $this->lang->line($controller_name."_none_selected")?>');
 });
 
-function init_table_sorting()
-{
+function init_table_sorting(){
 	//Only init if there is more than one row
-	if($('.tablesorter tbody tr').length >1)
-	{
-		$("#sortable_table").tablesorter(
-		{
-			sortList: [[1,0]],
-			headers:
-			{
-				0: { sorter: false},
-				3: { sorter: false}
+	if($('.tablesorter tbody tr').length >1){
+		$("#sortable_table").tablesorter({
+			sortList:[[1,0]],
+			headers:{
+				0:{sorter:false},
+				3:{sorter:false}
 			}
 		});
 	}
 }
 
-function post_giftcard_form_submit(response)
-{
-	if(!response.success)
-	{
+function post_giftcard_form_submit(response){
+	if(!response.success){
 		set_feedback(response.message,'error_message',true);
-	}
-	else
-	{
+	}else{
 		//This is an update, just update one row
-		if(jQuery.inArray(response.giftcard_id,get_visible_checkbox_ids()) != -1)
-		{
+		if(jQuery.inArray(response.giftcard_id,get_visible_checkbox_ids())!=-1){
 			update_row(response.giftcard_id,'<?php echo site_url("$controller_name/get_row")?>');
 			set_feedback(response.message,'success_message',false);
-
-		}
-		else //refresh entire table
-		{
-			do_search(true,function()
-			{
+		}else{ //refresh entire table
+			do_search(true,function(){
 				//highlight new row
 				hightlight_row(response.giftcard_id);
 				set_feedback(response.message,'success_message',false);
@@ -81,6 +65,5 @@ function post_giftcard_form_submit(response)
 		}
 	}
 }
-
 </script>
 <?php $this->load->view("partial/footer"); ?>

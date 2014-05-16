f963878d1b37f2331ed4ce4f05816fd9e6d58d36
ramonJR <maxtri@hotmail.com>
2014-05-16 09:57:18 -0430

    no_access new design

--- application/views/no_access.php
+++ application/views/no_access.php
@@ -1,5 +1,32 @@
-<?php
-$this->load->view("partial/header");
-echo $this->lang->line('error_no_permission_module').' '.$module_name; 
-$this->load->view("partial/footer");
-?> 
+<?php $this->load->view("partial/header"); ?>
+<style>
+	section.no-access{
+		background-color:#fff;
+		width: 900px;
+		height: 500px;
+		margin: auto;
+		border-radius: 10px;
+		padding: 10px;
+	}
+	section.no-access div.photo_add,section.no-access p{
+		display: inline-block;
+		margin: 50px 25px 0px;
+	}
+	section.no-access div.photo_add{
+		border: 1px transparent solid;
+		width: 330px; height: 300px;
+	}
+	section.no-access p{
+		width: 300px;
+		font-size: 20px;
+		font-weight: bold;
+		margin-bottom: 100px;
+	}
+</style>
+	<section class="no-access">
+		<div class="photo_add">
+			<div style="background-image: url('./images/menubar/logout.png'); background-size: 65%;"></div>
+		</div>
+		<p><?php echo $this->lang->line('error_no_permission_module').' '.$module_name; ?></p>
+	</section>
+<?php $this->load->view("partial/footer"); ?> 

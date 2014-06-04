14d71b8caf9367e498032dce70131e3033468ce9
miharbi <miharbihernandez@gmail.com>
2014-06-04 09:01:34 -0430

    sss

--- application/config/database.php
+++ application/config/database.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
-| DATABASE CONNECTIVITY SETTINGS
+| DATABASE CONNECTIVITY SETTINGS 
 | -------------------------------------------------------------------
 | This file will contain the settings needed to access your database.
 |

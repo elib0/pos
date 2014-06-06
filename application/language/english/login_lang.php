cf34625a0f038b335bd60b3a839eb80a79bb6a88
elibo <elijose.c@gmail.com>
2014-06-06 16:06:39 -0430

    Traduccion corregida de mensajes de error en login

--- application/language/english/login_lang.php
+++ application/language/english/login_lang.php
@@ -3,7 +3,7 @@ $lang['login_login']='Login';
 $lang['login_username']='Username';
 $lang['login_password']='Password';
 $lang['login_go']='Go';
-$lang['login_invalid_username_and_password']='Invalid username/password or you\'re out of time!';
+$lang['login_invalid_username_and_password']='Invalid username or password!';
 $lang['login_invalid_time']='You\'re out of time! Please contact the administrator!';
 $lang['login_welcome_message']='Welcome to the Open Source Point of Sale System. To continue, please login using your username and password below.';
 ?>

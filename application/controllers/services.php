b6d18857b28a4ba7bcc1187bfe55e1f289939a54
elibo <elijose.c@gmail.com>
2014-06-13 16:57:20 -0430

    Borrado de metodos que no se usaran

--- application/controllers/services.php
+++ application/controllers/services.php
@@ -8,25 +8,6 @@ class Services extends CI_Controller
 		parent::__construct('items');
 	}
 
-	function set_location(){
-		$location = $this->session->userdata('dblocation');
-		if($this->input->post('items_location')!=false){
-			$location = $this->input->post('items_location');
-		}elseif ($this->session->userdata('items_location')!=false) {
-			$location = $this->session->userdata('items_location');
-		}
-
-		//Establezco variable de session 
-		$this->session->set_userdata('items_location', $location);
-
-		//Llamado ajax
-		if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
-			die($location);
-		}else{
-			return $location;
-		}
-	}
-
 	function index()
 	{
 		$config['base_url'] = site_url('/items/index');
@@ -89,22 +70,13 @@ class Services extends CI_Controller
 	*/
 	function suggest()
 	{
-		$suggestions = $this->Item->get_search_suggestions($this->input->post('q'),$this->input->post('limit'));
+		$suggestions = $this->Service->suggest($this->input->post('q'),$this->input->post('limit'));
 		echo implode("\n",$suggestions);
 	}
 
 	function item_search()
 	{
 		$suggestions = $this->Item->get_item_search_suggestions($this->input->post('q'),$this->input->post('limit'));
-		echo implode("\n",$suggestions);
-	}
-
-	/*
-	Gives search suggestions based on what is being searched for
-	*/
-	function suggest_category()
-	{
-		$suggestions = $this->Item->get_category_suggestions($this->input->post('q'));
 		echo implode("\n",$suggestions);
 	}
 
@@ -132,55 +104,11 @@ class Services extends CI_Controller
 		$this->load->view("items/form",$data);
 	}
 
-	//Ramel Inventory Tracking
-	function inventory($item_id=-1)
-	{
-		$data['item_info']=$this->Item->get_info($item_id);
-		$this->load->view("items/inventory",$data);
-	}
-
 	function count_details($item_id=-1)
 	{
 		$data['item_info']=$this->Item->get_info($item_id);
 		$this->load->view("items/count_details",$data);
 	} //------------------------------------------- Ramel
-
-	function generate_barcodes($item_ids)
-	{
-		$result = array();
-
-		$item_ids = explode(':', $item_ids);
-		foreach ($item_ids as $item_id)
-		{
-			$item_info = $this->Item->get_info($item_id);
-
-			$result[] = array('name' =>$item_info->name, 'id'=> $item_id);
-		}
-
-		$data['items'] = $result;
-		$this->load->view("barcode_sheet", $data);
-	}
-
-	function bulk_edit()
-	{
-		$data = array();
-		$suppliers = array('' => $this->lang->line('items_none'));
-		foreach($this->Supplier->get_all()->result_array() as $row)
-		{
-			$suppliers[$row['person_id']] = $row['first_name'] .' '. $row['last_name'];
-		}
-		$data['suppliers'] = $suppliers;
-		$data['allow_alt_desciption_choices'] = array(
-			''=>$this->lang->line('items_do_nothing'),
-			1 =>$this->lang->line('items_change_all_to_allow_alt_desc'),
-			0 =>$this->lang->line('items_change_all_to_not_allow_allow_desc'));
-
-		$data['serialization_choices'] = array(
-			''=>$this->lang->line('items_do_nothing'),
-			1 =>$this->lang->line('items_change_all_to_serialized'),
-			0 =>$this->lang->line('items_change_all_to_unserialized'));
-		$this->load->view("items/form_bulk", $data);
-	}
 
 	function save($item_id=-1)
 	{
@@ -264,239 +192,7 @@ class Services extends CI_Controller
 
 	}
 
-	//Ramel Inventory Tracking
-	function save_inventory($item_id=-1, $db=null)
-	{
-		$employee_id=$this->Employee->get_logged_in_employee_info()->person_id;
-		$cur_item_info = $this->Item->get_info($item_id);
-		$inv_data = array
-		(
-			'trans_date'=>date('Y-m-d H:i:s'),
-			'trans_items'=>$item_id,
-			'trans_user'=>$employee_id,
-			'trans_comment'=>$this->input->post('trans_comment'),
-			'trans_inventory'=>$this->input->post('newquantity')
-		);
-		$this->Inventory->insert($inv_data);
-
-		//Comprueba si hay otra bd presente para tranferencia de items entre bds
-		$db = $this->input->post('dbselected');
-
-		//Si hay otra bd presente resto por que esta haciendo tranferencia de item
-		if ($db != '...' && $db != false){
-			if($this->input->post('newquantity') < $cur_item_info->quantity){ //Para que no pase mas de lo que tiene
-				$item_data = array(
-					'quantity' => $this->input->post('newquantity'),
-					'name' => $cur_item_info->name
-				);
-
-				if( $this->Item->save_in_other_inventory($item_data,$item_id,$db) ){ 	//Si incremento en la otra tienda
-					$item_data['quantity'] = $cur_item_info->quantity + ($this->input->post('newquantity') * -1);
-					$this->Item->save($item_data,$item_id);
-					echo json_encode(array('success'=>true,'message'=>'Item traspasest To'.' "'.$db.'" '.
-					$cur_item_info->name,'item_id'=>$item_id));
-				}else{																//Si no icremente en la otra tienda
-					echo json_encode(array('success'=>false,'message'=>'This item may not exist in the destination or location that no longer exists'));
-				}
-			}else{
-				echo json_encode(array('success'=>false,'message'=>'We can not transfer this amount'));
-			}
-		} else {
-			$newquantity = $this->input->post('newquantity');
-			//Update stock quantity
-			$item_data = array(
-				'quantity'=>$cur_item_info->quantity + $newquantity
-			);
-
-			if($this->Item->save($item_data,$item_id))
-			{
-				echo json_encode(array('success'=>true,'message'=>$this->lang->line('items_successful_updating').' '.
-				$cur_item_info->name,'item_id'=>$item_id));
-			}else{
-				echo json_encode(array('success'=>false,'message'=>$this->lang->line('items_error_adding_updating').' '.
-				$cur_item_info->name,'item_id'=>-1));
-			}
-		}
-
-	}//---------------------------------------------------------------------Ramel
-
-	function bulk_update()
-	{
-		$items_to_update=$this->input->post('item_ids');
-		$item_data = array();
-
-		foreach($_POST as $key=>$value)
-		{
-			//This field is nullable, so treat it differently
-			if ($key == 'supplier_id')
-			{
-				$item_data["$key"]=$value == '' ? null : $value;
-			}
-			elseif($value!='' and !(in_array($key, array('item_ids', 'tax_names', 'tax_percents'))))
-			{
-				$item_data["$key"]=$value;
-			}
-		}
-
-		//Item data could be empty if tax information is being updated
-		if(empty($item_data) || $this->Item->update_multiple($item_data,$items_to_update))
-		{
-			$items_taxes_data = array();
-			$tax_names = $this->input->post('tax_names');
-			$tax_percents = $this->input->post('tax_percents');
-			for($k=0;$k<count($tax_percents);$k++)
-			{
-				if (is_numeric($tax_percents[$k]))
-				{
-					$items_taxes_data[] = array('name'=>$tax_names[$k], 'percent'=>$tax_percents[$k] );
-				}
-			}
-			$this->Item_taxes->save_multiple($items_taxes_data, $items_to_update);
-			echo json_encode(array('success'=>true,'message'=>$this->lang->line('items_successful_bulk_edit')));
-		}
-		else
-		{
-			echo json_encode(array('success'=>false,'message'=>$this->lang->line('items_error_updating_multiple')));
-		}
-	}
-
-	function delete()
-	{
-		$items_to_delete=$this->input->post('ids');
-
-		if($this->Item->delete_list($items_to_delete))
-		{
-			echo json_encode(array('success'=>true,'message'=>$this->lang->line('items_successful_deleted').' '.
-			count($items_to_delete).' '.$this->lang->line('items_one_or_multiple')));
-		}
-		else
-		{
-			echo json_encode(array('success'=>false,'message'=>$this->lang->line('items_cannot_be_deleted')));
-		}
-	}
-
-	function excel()
-	{
-		$data = file_get_contents("import_items.csv");
-		$name = 'import_items.csv';
-		force_download($name, $data);
-	}
-
-	function excel_import()
-	{
-		$this->load->view("items/excel_import", null);
-	}
-
-	function do_excel_import()
-	{
-		$msg = 'do_excel_import';
-		$failCodes = array();
-		if ($_FILES['file_path']['error']!=UPLOAD_ERR_OK)
-		{
-			$msg = $this->lang->line('items_excel_import_failed');
-			echo json_encode( array('success'=>false,'message'=>$msg) );
-			return;
-		}
-		else
-		{
-			if (($handle = fopen($_FILES['file_path']['tmp_name'], "r")) !== FALSE)
-			{
-				//Skip first row
-				fgetcsv($handle);
-
-				$i=1;
-				while (($data = fgetcsv($handle)) !== FALSE)
-				{
-					$item_data = array(
-					'name'			=>	$data[1],
-					'description'	=>	$data[13],
-					'location'		=>	$data[12],
-					'category'		=>	$data[2],
-					'cost_price'	=>	$data[4],
-					'unit_price'	=>	$data[5],
-					'quantity'		=>	$data[10],
-					'reorder_level'	=>	$data[11],
-					'supplier_id'	=>  $this->Supplier->exists($data[3]) ? $data[3] : null,
-					'allow_alt_description'=> $data[14] != '' ? '1' : '0',
-					'is_serialized'=>$data[15] != '' ? '1' : '0'
-					);
-					$item_number = $data[0];
-
-					if ($item_number != "")
-					{
-						$item_data['item_number'] = $item_number;
-					}
-
-					if($this->Item->save($item_data))
-					{
-						$items_taxes_data = null;
-						//tax 1
-						if( is_numeric($data[7]) && $data[6]!='' )
-						{
-							$items_taxes_data[] = array('name'=>$data[6], 'percent'=>$data[7] );
-						}
-
-						//tax 2
-						if( is_numeric($data[9]) && $data[8]!='' )
-						{
-							$items_taxes_data[] = array('name'=>$data[8], 'percent'=>$data[9] );
-						}
-
-						// save tax values
-						if(count($items_taxes_data) > 0)
-						{
-							$this->Item_taxes->save($items_taxes_data, $item_data['item_id']);
-						}
-
-							$employee_id=$this->Employee->get_logged_in_employee_info()->person_id;
-							$emp_info=$this->Employee->get_info($employee_id);
-							$comment ='Qty CSV Imported';
-							$excel_data = array
-								(
-								'trans_items'=>$item_data['item_id'],
-								'trans_user'=>$employee_id,
-								'trans_comment'=>$comment,
-								'trans_inventory'=>$data[10]
-								);
-								$this->db->insert('inventory',$excel_data);
-						//------------------------------------------------Ramel
-					}
-					else//insert or update item failure
-					{
-						$failCodes[] = $i;
-					}
-				}
-
-				$i++;
-			}
-			else
-			{
-				echo json_encode( array('success'=>false,'message'=>'Your upload file has no data or not in supported format.') );
-				return;
-			}
-		}
-
-		$success = true;
-		if(count($failCodes) > 1)
-		{
-			$msg = "Most items imported. But some were not, here is list of their CODE (" .count($failCodes) ."): ".implode(", ", $failCodes);
-			$success = false;
-		}
-		else
-		{
-			$msg = "Import items successful";
-		}
-
-		echo json_encode( array('success'=>$success,'message'=>$msg) );
-	}
-
-	function report_item_broken($item_id){
-		if($this->Item->report_broken($item_id)){
-			echo json_encode( array('success'=>true, 'message'=>'Item reported') );
-		}else{
-			echo json_encode( array('success'=>false, 'message'=>'Error') );
-		}
-	}
+	
 	function uploadImagen_photo($id,$e){ $a=0;
 		//carga de imagen
 		$config['upload_path'] = './images/items/'.$id;
@@ -572,12 +268,5 @@ class Services extends CI_Controller
 	get the width for the add/edit form
 	*/
 	function get_form_width(){ return '660/height:465'; }
-
-	function item_search()
-	{
-		$suggestions = $this->Item->get_item_search_suggestions($this->input->post('q'),$this->input->post('limit'));
-		$suggestions = array_merge($suggestions, $this->Item_kit->get_item_kit_search_suggestions($this->input->post('q'),$this->input->post('limit')));
-		echo implode("\n",$suggestions);
-	}
 
 }

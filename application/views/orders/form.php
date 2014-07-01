5230130154a2a546c43f0af52ddd19e51308dd11
elibo <elijose.c@gmail.com>
2014-07-01 16:00:23 -0430

    Arrreglo de validacion(Campo Vacio)

--- application/views/orders/form.php
+++ application/views/orders/form.php
@@ -1,7 +1,7 @@
 <div>
 <h2><?php echo $this->lang->line('orders_quantity_required') ?></h2>
 <div style="float:left;padding:10px;">
-	<input type="number" id="new-qty" value="1">
+	<input type="number" id="new-qty" min="1" value="1">
 	<p>Press enter or OK button to accept changes...</p>
 	<input id="close_button" type="button" class="small_button" value="Ok">
 </div>
@@ -15,7 +15,8 @@
 			if (key>=96 && key<=105 || key>=48 && key<=57 || key>=37 && key<=40 || key==8 || key==46) {
 				pressed = true;
 			}else if(key==13){
-				if ($(that).val() == '' || $(that).val() == 0) {$(that).val('1')};
+				pressed = true;
+				if ( $(that).val().length < 1 ) {$(that).val('1')};
 				tb_remove(false);
 			}else{
 				e.preventDefault();
